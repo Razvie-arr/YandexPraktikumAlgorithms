333d8773741b3d93afb846978fb437d7dadbf934
Your Name <you@example.com>
2021-06-24 00:03:02 +0900

    нахождение палиндрома прошло все тесты

--- src/com/company/Palindromes.java
+++ src/com/company/Palindromes.java
@@ -3,35 +3,45 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Palindromes {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String phrase = reader.readLine();
-        int lenghtOfPhrase = phrase.length();
-        Pattern patkirletter = Pattern.compile("[а-яА-Я]{1}");
-        Matcher matkirletter = patkirletter.matcher(phrase);
-        //проверка на длину (должно быть меньше или равно 20000)
-        if (lenghtOfPhrase > 20000) {
-            System.exit(0);
+        if (isPalindrome(phrase)) {
+            System.out.println("True");
         }
-        else if (matkirletter.matches()) {
+        else {
+            System.out.println("False");
+        }
+
+    }
+    public static boolean isPalindrome(String phrase) {
+        if (!phrase.matches("^[A-Za-z0-9-.?!)(,: ]+$")) {
             System.exit(0);
         }
         else {
-            StringBuilder phraseSB = new StringBuilder();
-            for (int i = 0; i < lenghtOfPhrase; i++) {
-                if (Character.isLetterOrDigit(phrase.charAt(i))) {
-                    phraseSB.append(phrase.charAt(i));
-                }
+            int lenghtOfPhrase = phrase.length();
+            //проверка на длину (должно быть меньше или равно 20000)
+            if (lenghtOfPhrase > 20000) {
+                System.exit(0);
             }
-            String phraseWithoutSymbols = phraseSB.toString().toLowerCase();
-            isPalindrome(phraseWithoutSymbols);
+            else {
+                StringBuilder purePhrase = new StringBuilder();
+                StringBuilder reversedPhrase= new StringBuilder();
+                for (int i = 0; i < lenghtOfPhrase; i++) {
+                    if (Character.isLetterOrDigit(phrase.charAt(i))) {
+                        purePhrase.append(phrase.charAt(i));
+                        reversedPhrase.insert(0, phrase.charAt(i));
+                    }
+                }
+                String pureLower = purePhrase.toString().toLowerCase();
+                String reversedLower = reversedPhrase.toString().toLowerCase();
+
+                return pureLower.equals(reversedLower);
+            }
         }
+        return false;
     }
-    public static void isPalindrome(String input) {
-        System.out.println(input);
-    }
+
 }
