f27a3bb2c3261f88e9b79d80699fec9ec1d101f1
Your Name <you@example.com>
2021-06-28 00:22:31 +0900

    нахождение ближайшего нуля прошло все тесты!

--- src/com/company/ClosestZero.java
+++ src/com/company/ClosestZero.java
@@ -3,31 +3,35 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class ClosestZero {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
         if (n < 1 || n > 1000000) {
             System.exit(0);
         }
         int[] HouseNumbers = new int[n];
         //HashSet для того, чтобы туда вставлялись только уникальные номера домов
         HashSet <Integer> uniqHouseNumbers = new HashSet<>();
-        ArrayList <Integer> HouseNumbersWithoutZero = new ArrayList<>();
+        ArrayList<Integer> HouseNumbersWithoutZero = new ArrayList<>();
         int zero_counter = 0;
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
         for (int i = 0; i < n; i++) {
             int house_number = Integer.parseInt(tokenizer.nextToken());
             if (house_number < 0 || house_number > 1000000000) {
                 System.exit(0);
             }
-            else if (house_number == 0) {
+            HouseNumbers[i] = house_number;
+            if (house_number == 0) {
                 zero_counter += 1;
             }
-            HouseNumbers[i] = house_number;
-            if (HouseNumbers[i] != 0) {
+            else {
                 HouseNumbersWithoutZero.add(house_number);
                 uniqHouseNumbers.add(house_number);
             }
@@ -36,38 +40,37 @@ public class ClosestZero {
             System.exit(0);
         }
         //сравниваем длину HashSet с длиной массива, который не включает в себя нули, если эти длины не равны, значит есть неуникальные элементы
-        int withoutZeroCounter = 0;
-        for(int i = 0; i < HouseNumbersWithoutZero.size(); i++) {
-           withoutZeroCounter += 1;
-        }
-        if (withoutZeroCounter != uniqHouseNumbers.size()) {
+        if (HouseNumbersWithoutZero.size() != uniqHouseNumbers.size()) {
             System.exit(0);
         }
-        findClosestZero(HouseNumbers);
+        findClosestZero(HouseNumbers, n);
     }
-    public static void findClosestZero(int[] input) {
-        int input_lenght = input.length;
-        int[] left = new int[input_lenght];
-        int[] right = new int[input_lenght];
-        int[] result = new int[input_lenght];
+    public static void findClosestZero(int[] input, int n) {
+        StringBuilder result = new StringBuilder();
+        int[] left = new int[n];
+        int[] right = new int[n];
 
-        for (int i = 0; i < input_lenght; i++)
+        for (int i = 0; i < n; i++)
         {
-            left[i] = input[i] == 0 ? 0 : input_lenght;
-            right[i] = input[i] == 0 ? 0 : input_lenght;
+            left[i] = input[i] == 0 ? 0 : n;
+            right[i] = left[i];
+            if (i > 0) {
+                left[i] = input[i] == 0 ? 0 : left[i - 1] + 1;
+            }
         }
 
-        for (int i = 1; i < input_lenght; i++)
-            left[i] = input[i] == 0 ? 0 : left[i - 1] + 1;
-
-        for (int i = input_lenght - 2; i >= 0; i--)
+        for (int i = n - 2; i >= 0; i--)
             right[i] = input[i] == 0 ? 0 : right[i + 1] + 1;
 
-        for (int i = 0; i < input_lenght; i++)
+        for (int i = 0; i < n; i++)
         {
-            result[i] = Math.min(left[i], right[i]);
-            System.out.print(result[i] + " ");
+            if (left[i] < right[i]) {
+               result.append(left[i]).append(" ");
+            }
+            else {
+                result.append(right[i]).append(" ");
+            }
         }
-
+        System.out.print(result);
     }
 }
