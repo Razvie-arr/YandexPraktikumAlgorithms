0a2717ec72921ffed9fe4cbb5c6da3159d589a88
Your Name <you@example.com>
2021-07-25 22:13:54 +0900

    дек готов, но пока не проходит по времени

--- src/com/company/Final1_2.java
+++ src/com/company/Final1_2.java
@@ -1,49 +1,35 @@
 package com.company;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 class LimitedDeque2 {
-    Integer[] deque;
-    int maxSize;
+    int[] deque;
     int head;
-    int tail;
     int size;
+    int maxSize;
 
-    LimitedDeque2(int max_size) {
-        deque = new Integer[max_size];
-        Arrays.fill(deque, null);
-        this.maxSize = max_size;
-        head = 0;
-        tail = deque.length - 1;
-        size = 0;
+    public LimitedDeque2(int maxSize) {
+        deque = new int[maxSize];
+        this.maxSize = maxSize;
     }
 
-    public void push_front(int number) {
+    public void push_back(int number) {
         if (size != maxSize) {
-            deque[head] = number;
-            if (head == deque.length - 1) {
-                head = 0;
-            }
-            else {
-                head = head + 1 % maxSize;
-            }
-            size++;
+            deque[(head + size++) % maxSize] = number;
         }
         else {
             System.out.println("error");
         }
     }
 
-    public void push_back(int number) {
+    public void push_front(int number) {
         if (size != maxSize) {
-            deque[tail] = number;
-            if (tail == 0) {
-                tail = deque.length - 1;
+            if (head == 0) {
+                deque[(head = maxSize - 1)] = number;
+            } else {
+                deque[(--head) % maxSize] = number;
             }
-            else {
-                tail = (tail - 1) % maxSize;
-            }
+
             size++;
         }
         else {
@@ -52,38 +38,16 @@ class LimitedDeque2 {
     }
 
     public int pop_front() {
-        if (head == 0) {
-            head = tail;
-            int number = deque[head + 1];
-            deque[head + 1] = null;
-            head = (head - 1) % maxSize;
-            size--;
-            return number;
-        }
-        else {
-            int number = deque[head - 1];
-            deque[head - 1] = null;
-            head = (head - 1) % maxSize;
-            size--;
-            return number;
-        }
+        int number = deque[head];
+        head = (head + 1) % maxSize;
+        size--;
+        return number;
     }
 
     public int pop_back() {
-        if (tail == deque.length - 1) {
-            tail = 0;
-            int number = deque[tail];
-            deque[tail] = null;
-            size--;
-            return number;
-        }
-        else {
-            int number = deque[tail + 1];
-            deque[tail + 1] = null;
-            tail = (tail + 1) % maxSize;
-            size--;
-            return number;
-        }
+        int number = deque[(head + size - 1) % maxSize];
+        size--;
+        return number;
     }
 }
 
@@ -99,42 +63,48 @@ public class Final1_2 {
 
         for (int i = 0; i < n; i++) {
             String command = reader.readLine();
+
             if (command.contains("push_back")) {
                 int value = findNumberInCommand(command);
                 validateValue(value);
                 deque.push_back(value);
             }
+
             else if (command.contains("push_front")) {
                 int value = findNumberInCommand(command);
                 validateValue(value);
                 deque.push_front(value);
             }
+
             else {
-                if (deque.size == 0) {
-                    System.out.println("error");
-                }
-                else {
-                    switch (command) {
-                        case("pop_front"):
-                            System.out.println(deque.pop_front());
-                            break;
-                        case("pop_back"):
-                            System.out.println(deque.pop_back());
-                            break;
+                    if (deque.size == 0) {
+                        System.out.println("error");
+                    }
+                    else {
+                        switch (command) {
+                            case("pop_front"):
+                                System.out.println(deque.pop_front());
+                                break;
+                            case("pop_back"):
+                                System.out.println(deque.pop_back());
+                                break;
+                        }
                     }
                 }
             }
         }
-    }
+
     private static int findNumberInCommand(String command) {
         String[] splittedCommand = command.split(" ");
         return Integer.parseInt(splittedCommand[1]);
     }
+
     public static void validateConditions(int n, int m) throws Exception {
         if (n > 5000 || m > 1000) {
             throw new Exception();
         }
     }
+
     public static void validateValue(int value) throws Exception {
         if (Math.abs(value) > 1000) {
             throw new Exception();
