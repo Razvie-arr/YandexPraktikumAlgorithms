c00f97be4d5d4dba6f248aaf1e4ae6891f2f87de
razviearr <vladbyastinov@mail.ru>
2021-08-17 01:18:55 +0900

    в процессе поиск в сломанном массиве, нужно как-то отдавать нескопированный массив

--- src/com/company/sprint3/Solution.java
+++ src/com/company/sprint3/Solution.java
@@ -3,28 +3,48 @@ package com.company.sprint3;
 import java.util.Arrays;
 
 public class Solution {
-    public static int brokenSearch(int[] arr, int k) {
-        Arrays.sort(arr);
+    public static int brokenSearch(int[] arr, int k) { ;
         int left = 0;
         int right = arr.length - 1;
-        int index = -1;
-        if (arr[left] <=)
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == k) {
-                index = mid;
-                break;
-            }
-            else if (arr[mid] < k) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
+        int mid = arr.length / 2;
+        if (arr[mid] == k) {
+            return mid;
+        }
+        else if (arr.length == 1) {
+            if (arr[0] == k) {
+                return k;
             }
         }
-        return index;
+        else if (arr[left] <= arr[mid]) {
+            if (k <= arr[mid]) {
+                return brokenSearch(Arrays.copyOfRange(arr,0, mid), k);
+            }
+            else {
+                return brokenSearch(Arrays.copyOfRange(arr, mid, right + 1), k);
+            }
+        }
+        else {
+            if (k <= arr[mid]) {
+                right = mid;
+                return brokenSearch(Arrays.copyOfRange(arr,0, right), k);
+            }
+            else {
+                int leftToMid= brokenSearch(Arrays.copyOfRange(arr,0, mid), k);
+                if (leftToMid != -1) {
+                    return leftToMid;
+                }
+                int midToRight = brokenSearch(Arrays.copyOfRange(arr, mid, right + 1), k);
 
+                if (midToRight != -1) {
+                    return midToRight + mid + 1;
+                }
+                else {
+                    return -1;
+                }
+            }
+        }
 
+        return -1;
     }
 
     public static void main(String[] args) {
