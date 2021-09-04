5d05d116e5eb120c064b639604010d2c6b17cd90
razviearr <vladbyastinov@mail.ru>
2021-09-05 04:43:33 +0900

    код ревью поиска в сломанном массиве - готово

--- src/com/company/sprint3/Solution.java
+++ src/com/company/sprint3/Solution.java
@@ -1,42 +1,67 @@
 package com.company.sprint3;
+/*
+https://contest.yandex.ru/contest/23815/run-report/52627515/
+-- ПРИНЦИП РАБОТЫ --
+Реализован метод  brokenSearch, который принимает массив arr и искомое число k (пришлось использовать перегрузку методов и дублировать код,
+т.к сигнатура заготовки не позволила использовать сразу метод, который принимает в себя arr, k, left, right).
+Далее ищем отсортированную сторону массива и используем на ней классический бинарный поиск
 
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+Поиск искомого элемента производится за O(log n)
+
+
+ */
 public class Solution {
     public static int brokenSearch(int[] arr, int k) {
         int left = 0;
         int right = arr.length - 1;
+        int mid = (left + right) / 2;
+        if (arr[mid] == k) {
+            return mid;
+        }
         if (right >= left) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == k) {
-                return mid;
+            //получается левая часть отсортирована
+            if (arr[left] <= arr[mid]) {
+                int leftToMid = binarySearch(arr, k, left, mid - 1);
+                if (leftToMid != - 1) {
+                    return leftToMid;
+                }
+                return brokenSearch(arr, k, mid + 1, right);
             }
-            int slice = sliceSearch(arr, left, right);
-            int leftToSlice = binarySearch(arr, k, left, slice - 1);
-            if (leftToSlice != -1) {
-                return leftToSlice;
-            } else {
-                return binarySearch(arr, k, slice, right);
+            else {
+                int rightToMid = binarySearch(arr, k, mid + 1, right);
+                if (rightToMid != -1) {
+                    return rightToMid;
+                }
+                return brokenSearch(arr, k, left, mid - 1);
             }
         }
         return -1;
     }
 
-    public static int sliceSearch(int[] arr, int left, int right) {
-        try {
-            int mid = (left + right) / 2;
-            if (arr[mid + 1] < arr[mid]) {
-                return mid + 1;
-            }
-            if (arr[left] > arr[mid]) {
-                int leftToMid = sliceSearch(arr, left, mid);
-                if (leftToMid != -1) {
-                    return leftToMid + 1;
+    public static int brokenSearch(int[] arr, int k, int left, int right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == k) {
+            return mid;
+        }
+        if (right >= left) {
+            //получается левая часть отсортирована
+            if (arr[left] <= arr[mid]) {
+                int leftToMid = binarySearch(arr, k, left, mid - 1);
+                if (leftToMid != - 1) {
+                    return leftToMid;
                 }
+                return brokenSearch(arr, k, mid + 1, right);
             }
-            return sliceSearch(arr, mid + 1, right) + 1;
+            else {
+                int rightToMid = binarySearch(arr, k, mid + 1, right);
+                if (rightToMid != -1) {
+                    return rightToMid;
+                }
+                return brokenSearch(arr, k, left, mid - 1);
+            }
         }
-        catch (ArrayIndexOutOfBoundsException e) {
-            return -1;
-        }
+        return -1;
     }
 
     public static int binarySearch(int[] sortedArray, int key, int low, int high) {
@@ -56,8 +81,4 @@ public class Solution {
         return index;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {12, 41, 122, 411, 412, 1222, 3000, 12222, 122222, 0, 1, 2, 3, 4, 5};
-        System.out.println(brokenSearch(arr, 3000));
-    }
 }
