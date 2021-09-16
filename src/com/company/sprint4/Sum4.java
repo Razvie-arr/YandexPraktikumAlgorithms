ecd818d7db32b34b299c8c284a7c9155f45fb9fd
razviearr <vladbyastinov@mail.ru>
2021-09-17 01:44:36 +0900

    в процессе лексикографическая сортировка строк из чисел

--- src/com/company/sprint4/Sum4.java
+++ src/com/company/sprint4/Sum4.java
@@ -3,8 +3,18 @@ package com.company.sprint4;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Sum4 {
+    static class Pair{
+        int i;
+        int j;
+
+        Pair(int x,int y){
+            i=x;
+            j=y;
+        }
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
@@ -26,38 +36,54 @@ public class Sum4 {
 
     public static ArrayList<Integer> getIntegerArrayList(String[] stringArr) {
         ArrayList<Integer> ret = new ArrayList<>();
-        for (int i = 0; i < stringArr.length; i++) {
-            ret.add(Integer.parseInt(stringArr[i]));
+        for (String s : stringArr) {
+            ret.add(Integer.parseInt(s));
         }
         return ret;
     }
 
     public static void effectiveSolution(int A, ArrayList<Integer> x) {
-        HashSet<Integer> history = new HashSet<>();
-        int n = x.size();
         Collections.sort(x);
+        int len = x.size();
         HashSet<List<Integer>> quad = new HashSet<>();
-        int target;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int z = j + 1; z <n; z ++)
-                {
-                    target = A - x.get(i) - x.get(j) - x.get(z);
-
-                    if (history.contains(target)) {
-                        quad.add(Arrays.asList(target, x.get(i), x.get(j), x.get(z)));
-                    }
+        Map<Integer, Pair> sums = new HashMap<>();
+        for (int i = 0; i < len; ++i) {
+            for (int j = i + 1; j < len; ++j) {
+                int current = x.get(i) + x.get(j);
+                int rest = A - current;
+                if (sums.containsKey(rest)) {
+                    Pair p = sums.get(rest);
+                    quad.add(Arrays.asList(x.get(p.j), x.get(p.i), x.get(i), x.get(j)));
                 }
             }
-            history.add(x.get(i));
+            for (int k = 0; k < i; ++k) {
+                sums.put(x.get(i) + x.get(k), new Pair(i, k));
+            }
         }
-
-        System.out.println(quad.size());
-        String quadString = quad.toString();
-        quadString = quadString.replace("[", "");
-        quadString = quadString.replace(",", "");
-        quadString = quadString.replace("] ", "\n");
-        quadString = quadString.replace("]", "");
-        System.out.println(quadString);
+        String sortedQuad = lexicoCompare(quad);
+        sortedQuad = sortedQuad.replaceAll("\\[|\\]", "");
+        System.out.println(sortedQuad.replace(", ", "\n"));
     }
+
+    public static String lexicoCompare(HashSet<List<Integer>> quad) {
+        List<String> output = new ArrayList<>();
+        for (List<Integer> elem : quad) {
+            String elemString = elem.toString();
+            elemString = elemString.replace(",", " ");
+            elemString = elemString.replaceAll("\\[|\\]", "");
+            output.add(elemString);
+        }
+        output = sortAsNumbers(output);
+        return output.toString();
+    }
+
+    public static List<String> sortAsNumbers(Collection<String> collection) {
+        return collection
+                .stream()
+                .map(Integer::valueOf)
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
+
 }
