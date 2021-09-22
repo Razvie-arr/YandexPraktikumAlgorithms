1ecdece789e9ba3d38fac1407fc8a8e6247d988d
razviearr <vladbyastinov@mail.ru>
2021-09-22 16:53:26 +0900

    задача про проверку на бинарное поисковое дерево

--- src/com/company/sprint5/Solution.java
+++ src/com/company/sprint5/Solution.java
@@ -1,27 +1,28 @@
 package com.company.sprint5;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    public static int treeSolution(Node head) {
-        int maxValue = head.value;
+    public static boolean treeSolution(Node head) {
         Queue<Node> queue = new LinkedList<>();
         queue.add(head);
         while(queue.size() > 0){
             Node tempNode = queue.poll();
-            if (tempNode.value > maxValue) {
-                maxValue = tempNode.value;
-            }
             if (tempNode.left != null){
+                if (tempNode.left.value >= tempNode.value) {
+                    return false;
+                }
                 queue.add(tempNode.left);
             }
             if (tempNode.right != null){
+                if (tempNode.right.value <= tempNode.value) {
+                    return false;
+                }
                 queue.add(tempNode.right);
             }
         }
-        return maxValue;
+        return true;
     }
 
     private static class Node {
@@ -34,18 +35,23 @@ public class Solution {
             this.left = null;
             this.right = null;
         }
+
+        Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
     }
 
 
     public static void main(String[] args) {
-        Node node1 = new Node(1);
-        Node node2 = new Node(-5);
-        Node node3 = new Node(3);
-        node3.left = node1;
-        node3.right = node2;
-        Node node4 = new Node(2);
-        node4.left = node3;
-        System.out.println(treeSolution(node4));
-        assert treeSolution(node4) == 3;
+        Node node6 = new Node(9, null, null);
+        Node node5 = new Node(6, null, null);
+        Node node4 = new Node(3, null, null);
+        Node node3 = new Node(1, null, null);
+        Node node2 = new Node( 8, node5, node6);
+        Node node1 = new Node(3, node3, node4);
+        Node node0 = new Node(5, node1, node2);
+        System.out.println(treeSolution(node0));
     }
 }
