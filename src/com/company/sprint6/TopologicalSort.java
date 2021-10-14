3b95786291a18934b338ccd25169c8a5b45eb054
razviearr <vladbyastinov@mail.ru>
2021-10-14 22:13:07 +0200

    топологическая сортировка графа прошла по времени!

--- src/com/company/sprint6/TopologicalSort.java
+++ src/com/company/sprint6/TopologicalSort.java
@@ -33,24 +33,15 @@ public class TopologicalSort {
     public static String getTopSorted(TopGraph graph, Color[] colors) {
         StringBuilder out = new StringBuilder();
         Deque<Integer> order = new ArrayDeque<>();
-        long topSortStartTime = System.currentTimeMillis();
         for (int i = 1; i < colors.length; i++) {
             if (colors[i] == Color.WHITE) {
                 topSort(i, graph, colors, order);
             }
         }
-        long topSortEndTime = System.currentTimeMillis();
-        System.out.println("Скорость работы topSort для каждой вершины с рекурсией в мс: " + (topSortEndTime - topSortStartTime));
-        long appendOrderToOutStartTime = System.currentTimeMillis();
         while (!order.isEmpty()) {
             out.append(order.pop()).append(" ");
         }
-        long appendOrderToOutEndTime = System.currentTimeMillis();
-        System.out.println("Скорость добавления order в out в мс: " + (appendOrderToOutEndTime - appendOrderToOutStartTime));
-        long outDeleteStartTime = System.currentTimeMillis();
         out.delete(out.length() - 1, out.length());
-        long outDeleteEndTime = System.currentTimeMillis();
-        System.out.println("Скорость удаления первого и последнего символа в out в мс: " + (outDeleteEndTime - outDeleteStartTime));
         return out.toString();
     }
 
@@ -74,38 +65,17 @@ public class TopologicalSort {
         int n = Integer.parseInt(numbers[0]);
         int m = Integer.parseInt(numbers[1]);
         TopGraph graph = new TopGraph(n);
-        long fillGraphStartTime = System.currentTimeMillis();
-        long ribSplitTime = 0;
-        long fromToTime = 0;
-        long graphAddTime = 0;
         for (int i = 0; i < m; i++) {
-            long ribSplitStartTime = System.currentTimeMillis();
-            String[] rib = reader.readLine().trim().split("\\s+");
-            long ribSplitEndTime = System.currentTimeMillis();
-            ribSplitTime = ribSplitTime + (ribSplitEndTime - ribSplitStartTime);
-            long fromToStartTime = System.currentTimeMillis();
-            int from = Integer.parseInt(rib[0]);
-            int to = Integer.parseInt(rib[1]);
-            long fromToEndTime = System.currentTimeMillis();
-            fromToTime = fromToTime + (fromToEndTime - fromToStartTime);
-            long graphAddStartTime = System.currentTimeMillis();
+            StringTokenizer st = new StringTokenizer(reader.readLine()," ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
             graph.addEdge(from, to);
-            long graphAddEndTime = System.currentTimeMillis();
-            graphAddTime = graphAddTime + (graphAddEndTime - graphAddStartTime);
         }
-        long fillGraphEndTime = System.currentTimeMillis();
-        System.out.println("Скорость работы добавления вершин в мс: " + (fillGraphEndTime - fillGraphStartTime));
-        System.out.println("Скорость сплита ребер мс: " + ribSplitTime);
-        System.out.println("Скорость фромту в мс: " + fromToTime);
-        System.out.println("Скорость графадд в мс: " + (fillGraphEndTime - fillGraphStartTime));
-        long fillColorStartTime = System.currentTimeMillis();
         Color[] colors = new Color[n + 1];
         for (int i = 1; i < n + 1 ; i++) {
             colors[i] = Color.WHITE;
         }
-        long fillColorEndTime = System.currentTimeMillis();
-        System.out.println("Скорость работы добавления цветов в мс: " + (fillColorEndTime - fillColorStartTime));
-        getTopSorted(graph, colors);
+        System.out.println(getTopSorted(graph, colors));
     }
 
 
