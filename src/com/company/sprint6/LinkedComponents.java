7504db08f8923fa297bd7bae80d45c92f7a04b9e
razviearr <vladbyastinov@mail.ru>
2021-10-14 22:59:13 +0200

    покрыл связность компоненты замерами времени

--- src/com/company/sprint6/LinkedComponents.java
+++ src/com/company/sprint6/LinkedComponents.java
@@ -1,6 +1,6 @@
 package com.company.sprint6;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.FileReader;
 import java.util.*;
 
 class LinkGraph {
@@ -45,11 +45,11 @@ public class LinkedComponents {
             if (connectedComponents.containsKey(colors[i])) {
                 connectedComponents.get(colors[i]).add(i);
             } else {
-                connectedComponents.put(colors[i], new ArrayList<>(Arrays.asList(i)));
+                connectedComponents.put(colors[i], new ArrayList<>(Collections.singletonList(i)));
             }
         }
-        connectedComponents.entrySet().forEach(entry -> {
-            String values = entry.getValue().toString().replace(",", "");
+        connectedComponents.forEach((key, value) -> {
+            String values = value.toString().replace(",", "");
             out.append(values, 1, values.length() - 1).append("\n");
         });
         out.delete(out.length() - 1, out.length());
@@ -57,10 +57,10 @@ public class LinkedComponents {
     }
 
     public static void linkSort(int v, LinkGraph graph, int[] colors, int componentCount) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(v);
-        while (!stack.isEmpty()) {
-            int tempVertex = stack.pop();
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.push(v);
+        while (!deque.isEmpty()) {
+            int tempVertex = deque.pop();
             if (colors[tempVertex] == -1) {
                 colors[tempVertex] = componentCount;
                 List<Integer> edges = graph.getEdges(v);
@@ -76,22 +76,50 @@ public class LinkedComponents {
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
-        String[] numbers = reader.readLine().trim().split("\\s+");
-        int n = Integer.parseInt(numbers[0]);
-        int m = Integer.parseInt(numbers[1]);
+        BufferedReader reader = new BufferedReader((new FileReader("/Users/razviearr/PetProjects/Yandex-Praktikum-Algorithms/src/com/company/sprint6/test.txt")));
+        StringTokenizer numbers = new StringTokenizer(reader.readLine()," ");
+        int n = Integer.parseInt(numbers.nextToken());
+        int m = Integer.parseInt(numbers.nextToken());
         LinkGraph graph = new LinkGraph(n);
+        long graphFillStart = System.currentTimeMillis();
+
+        long tokenizerFromToTime = 0;
+        long graphAddEdgeTime = 0;
         for (int i = 0; i < m; i++) {
-            String[] rib = reader.readLine().trim().split("\\s+");
-            int from = Integer.parseInt(rib[0]);
-            int to = Integer.parseInt(rib[1]);
+            long tokenStart = System.currentTimeMillis();
+            StringTokenizer st = new StringTokenizer(reader.readLine()," ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            long tokenEnd = System.currentTimeMillis();
+            tokenizerFromToTime = tokenizerFromToTime + (tokenEnd - tokenStart);
+            long graphStart = System.currentTimeMillis();
             graph.addEdge(from, to);
+            long graphEnd = System.currentTimeMillis();
+            graphAddEdgeTime = graphAddEdgeTime + (graphEnd - graphStart);
         }
+
+        long graphFillEnd = System.currentTimeMillis();
+        long fillColorStart = System.currentTimeMillis();
         int[] colors = new int[n + 1];
-        for (int i = 1; i < n + 1 ; i++) {
+        for (int i = 1; i < n + 1; i++) {
             colors[i] = -1;
         }
-        System.out.println(getLinkedSorted(graph, colors));
+        long fillColorEnd = System.currentTimeMillis();
+        long sortStart = System.currentTimeMillis();
+        String linkedSorted = getLinkedSorted(graph, colors);
+        long sortEnd = System.currentTimeMillis();
+        long printComponentStart = System.currentTimeMillis();
+        System.out.println(linkedSorted);
+        long printComponentEnd = System.currentTimeMillis();
+
+        System.out.println("Создание вершин для графа: " + (graphFillEnd - graphFillStart));
+        System.out.println("Создание и заполнение массива с цветами: " + (fillColorEnd - fillColorStart));
+        System.out.println("Основной алгоритм: " + (sortEnd - sortStart));
+        System.out.println("Время вывода: " + (printComponentEnd - printComponentStart));
+        System.out.println();
+        System.out.println("Новые данные");
+        System.out.println("Токен и фромту: " + tokenizerFromToTime);
+        System.out.println("Заполнение графа: " + graphAddEdgeTime);
     }
 
 
