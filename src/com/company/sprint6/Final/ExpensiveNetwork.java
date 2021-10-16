416b87b3ce1e71de978050985ffef01a3fd3fc61
razviearr <vladbyastinov@mail.ru>
2021-10-16 17:45:23 +0200

    задача с дорогой сетью в процессе

---
+++ src/com/company/sprint6/Final/ExpensiveNetwork.java
@@ -0,0 +1,63 @@
+package com.company.sprint6.Final;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class ExpensiveGraph {
+    Map<Integer, List<List<Integer>>> graph;
+    public ExpensiveGraph(int n) {
+        this.graph = new HashMap<>(n);
+    }
+
+    public void addVertex(int s)
+    {
+        graph.put(s, new ArrayList<>());
+    }
+
+    public void addEdge(int from, int to, int weight) {
+
+        if (!graph.containsKey(from))
+            addVertex(from);
+        if (!graph.containsKey(to))
+            addVertex(to);
+        graph.get(from).add(new ArrayList<>(Arrays.asList(to, weight)));
+        graph.get(to).add(new ArrayList<>(Arrays.asList(from, weight)));
+    }
+
+    public List<List<Integer>> getEdges(int vertex) {
+        return graph.get(vertex);
+    }
+}
+public class ExpensiveNetwork {
+    public static void addVertex(int vertex, List<Integer> added, Set<Integer> notAdded, List<Integer> edges) {
+        added.add(vertex);
+        notAdded.remove(vertex);
+
+    }
+
+    public static int getMinimumSpanningTree(Map<Integer, List<List<Integer>>> graph, int n) {
+        List<Integer> edges = new ArrayList<>();
+        List<Integer> added = new ArrayList<>(n);
+        Set<Integer> notAdded = new HashSet<>(graph.keySet());
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer numbers = new StringTokenizer(reader.readLine()," ");
+        int n = Integer.parseInt(numbers.nextToken());
+        int m = Integer.parseInt(numbers.nextToken());
+        ExpensiveGraph graph = new ExpensiveGraph(n);
+        //этап 1: заполнение графа
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine()," ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            graph.addEdge(from, to, weight);
+        }
+        System.out.println(getMinimumSpanningTree(graph, n));
+    }
+}
